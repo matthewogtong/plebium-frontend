b8480de8b3f35afde0e1d13967ebd934ff73c433
matthewogtong <ogtongm@gmail.com>
2021-01-24 21:11:13 -0500

    modal set up

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,13 +1,16 @@
 import React from "react";
-
+import Jumbotron from "react-bootstrap/Jumbotron"
+import Button from "react-bootstrap/Button"
 function MainHeader() {
     return (
-        <div className="main-header">
-            <h5>plebium</h5>
-            <button>Write</button>
-            <button>dropdown menu here</button> 
-        </div>
-    )
+      <div className="main-header">
+        <Jumbotron>
+          <h1 className="main-header-title">Plebium</h1><h2>Welcome Back</h2>
+          
+        <Button className="main-header-write" variant="success">Write</Button>
+        </Jumbotron>
+      </div>
+    );
         
 }
 
