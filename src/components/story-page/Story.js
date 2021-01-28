59c984bdefa520fc75e758f54d524744290badd8
Sara Cemal <you@example.com>
2021-01-27 23:43:55 -0500

    routes work minus component rendering

--- src/components/story-page/Story.js
+++ src/components/story-page/Story.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Story({storyTitle, storyContent, readTime}) {
+function Story({storyTopic, storyTitle, storyContent, readTime}) {
     // const {title, content, snaps, read_time, date_created} = story 
 
     return (
         <div className="story-div">
             <h1 className="story-title">{storyTitle}</h1>
             <p className="story-read-time">{readTime} minutes </p>
+            <p className="story-read-time">{storyTopic} </p>
             <h4 className="story-page-content">{storyContent}</h4>
 
         </div>
